95ddbb24c3c47a4057fedca23d34ea4add30f30b
shivam-shashank <63339321+shivam-shashank@users.noreply.github.com>
2022-10-13 17:30:34 +0530

    Update File1.py

--- File1.py
+++ File1.py
@@ -1,3 +1,3 @@
-print("NARUTO IS BETTER THAN BREAKING BAD123")
-print("kid.... watch it first")
+print("NARUTO IS BETTER THAN BREAKING GOOD123")
+print("kid.... watch it last")
 print("hi hf akjk vk v h fs f b")
